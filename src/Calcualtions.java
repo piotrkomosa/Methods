42e605f0da46ce8235c3c512bbe0f2fb26b39ff0
Piotr Komosa <komosa.piotr@gmail.com>
2019-10-03 23:05:53 +0200

    Update - Test class and Excerise

--- src/Calcualtions.java
+++ src/Calcualtions.java
@@ -1,32 +1,18 @@
 public class Calcualtions {
-
-    int number;
-    int number2;
-    int number3;
-    int number4;
-    boolean isEven;
-    boolean Odd;
-
     int circleField(int number) {
         double circleField = (number * Math.PI * number);
         return (int) circleField;
     }
-
-    int power(int number2) {
-        int power = number2 * number2;
+    int power(int number) {
+        int power = number * number;
         return power;
     }
-
-    boolean isEven(int number3) {
-        boolean isEven = number3 % 2 != 0;
+    boolean isEven(int number) {
+        boolean isEven = number % 2 == 0;
         return isEven;
     }
-    boolean isOdd(int number4) {
-        boolean isOdd = number4 % 2 == 0;
+    boolean isOdd(int number) {
+        boolean isOdd = number % 2 != 0;
         return isOdd;
     }
-
-
-
-
 }
--- src/Test.java
+++ src/Test.java
@@ -1,23 +1,13 @@
 public class Test {
     public static void main(String[] args) {
         Calcualtions mathCalc = new Calcualtions();
-        int area = mathCalc.circleField(4);
-        int number = 4;
-        System.out.println("Pole powierzchni koła o promieniu" + number +" to "+ area);
-
-        Calcualtions mathCalc2 = new Calcualtions();
-        int power = mathCalc2.power(7);
-        int number2=7;
-        System.out.println(number2 + " podniesiona do kwadratu to " + power);
-
-        Calcualtions mathCalc3 = new Calcualtions();
-        boolean isEven = mathCalc3.isEven(7);
-        int number3=9;
-        System.out.println("liczba " + number3 + " jest " + isEven);
-
-        Calcualtions mathCalc4 = new Calcualtions();
-        boolean isOdd = mathCalc4.isOdd(7);
-        int number4 =6;
-        System.out.println("liczba " + number4+ " jest " + isOdd);
+        int area = mathCalc.circleField(7);
+        System.out.println("Pole powierzchni koła o promieniu"+ 7 + area);
+        int power = mathCalc.power(7);
+        System.out.println(7 + " podniesiona do kwadratu to " + power);
+        boolean isEven = mathCalc.isEven(7);
+        System.out.println("liczba " + 7 + " jest " + isEven);
+        boolean isOdd = mathCalc.isOdd(7);
+        System.out.println("liczba " + 7 + " jest " + isOdd);
     }
 }
